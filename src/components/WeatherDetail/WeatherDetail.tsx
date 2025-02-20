f14104423f2bb868fbfba3f5f33d6de438c9f78f
Alexander Alvarez <125148489+itsxander7@users.noreply.github.com>
2025-02-20 09:48:06 -0600

    modified:   src/components/WeatherDetail/WeatherDetail.tsx

--- src/components/WeatherDetail/WeatherDetail.tsx
+++ src/components/WeatherDetail/WeatherDetail.tsx
@@ -12,8 +12,8 @@ export default function WeatherDetail({weather} : WeatherDetailProps) {
         <h2>Clima de: {weather.name}</h2>
         <p className={styles.current}>{ formatTemperature( weather.main.temp )}&deg;C</p>
         <div className={styles.temperatures}>
-            <p>Min: <span>{ formatTemperature( weather.main.temp_min )}&deg;C</span> </p>
-            <p>Max: <span>{ formatTemperature( weather.main.temp_max )}&deg;C</span> </p>
+            <p>Minima: <span>{ formatTemperature( weather.main.temp_min )}&deg;C</span> </p>
+            <p>Maxima: <span>{ formatTemperature( weather.main.temp_max )}&deg;C</span> </p>
         </div>
     </div>
   )
